c39a5813a5468a3f43f4bcfc303368574dbf750c
Harsh Bailurkar <harshbailurkar@gmail.com>
2024-07-27 10:02:30 +0530

    Delete routes directory

--- routes/livekitWebhook.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { handleLivekitWebhook } from "../controllers/livekitWebHook.controller.js";
-
-const router = Router();
-
-router.route("/livekit").post(handleLivekitWebhook);
-
-export default router;
--- routes/stream.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createIngress } from "../controllers/ingress.controller.js";
-
-const router = Router();
-
-router.route("/Ingress").post(createIngress);
-
-export default router;
